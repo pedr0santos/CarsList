e2d16543e83804b44d56395cd2713b62e864a49d
Pedro Santos <pedrosantos009@hotmail.com>
2024-09-14 12:44:39 -0300

    Feat: Preparando rotas, adicionando layout component, etc.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './core/layout/layout.component';
 
 export const routes: Routes = [
-  // { path: '', component: HomeComponent },
-  // {
-  //   path: 'vehicles',
-  //   loadChildren: () =>
-  //     import('./features/vehicles/vehicles.module').then((m) => m.VehiclesModule),
-  // },
-  // { path: '**', redirectTo: '', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent, // Envolvendo todas as rotas com o layout principal
+    children: [
+      { path: '', component: HomeComponent },
+    ],
+  },
+  { path: '**', redirectTo: '' },
 ];
--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
