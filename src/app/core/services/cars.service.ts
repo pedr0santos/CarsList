e6d925b4ab3a4a87931e4efe59ce363f6f61be37
Pedro Santos <pedrosantos009@hotmail.com>
2024-09-14 16:08:17 -0300

    Feat: Adicionando endpoint 'get vehicles'

--- src/app/core/services/cars.service.ts
+++ src/app/core/services/cars.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Vehicle } from '../models/cars-model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarsService {
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getVehicles(): Observable<Vehicle[]> {
+    return this.http.get<Vehicle[]>(this.apiUrl)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: any) {
+    console.error('Ocorreu um erro:', error);
+    return throwError(() => new Error('Algo deu errado; por favor, tente novamente.'));
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+  apiUrl: 'https://66e5b3d95cc7f9b6273e1e36.mockapi.io/api/'
+};
