4954c1f25bd1f611995e4fffbb8088c02522fbf5
Pedro Santos <pedrosantos009@hotmail.com>
2024-09-14 11:59:18 -0300

    Criando pastas, layout, enviroments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  // {
+  //   path: 'vehicles',
+  //   loadChildren: () =>
+  //     import('./features/vehicles/vehicles.module').then((m) => m.VehiclesModule),
+  // },
+  // { path: '**', redirectTo: '', pathMatch: 'full' },
+];
